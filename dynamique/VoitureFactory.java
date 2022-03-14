cc8b3d8170f8ec3461df066bca3fda882278080e
Vincent Gagnon <vincentgagnon53@gmail.com>
2022-03-14 18:25:03 -0400

    VoitureFactory half implemented

--- dynamique/VoitureFactory.java
+++ dynamique/VoitureFactory.java
@@ -1,13 +1,32 @@
 package dynamique;
 
 import voiture.Voiture;
+import voiture.VoitureSport;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class VoitureFactory {
-    //regarder TestFactory pour exemple
-
     public enum ModeConstruction {INSTANCIATION, REFLEXION, META};
 
-    public static Voiture buildVoiture(ModeConstruction mode, boolean sport, int vitesse) {
-        return new Voiture(0);
-    }
+    public static Voiture buildVoiture(ModeConstruction mode, boolean sport, int vitesse) throws {
+        if (mode == ModeConstruction.INSTANCIATION) {
+            if (sport) {
+                return new VoitureSport();
+            } else {
+                return new Voiture(vitesse);
+            }
+        } else if(mode == ModeConstruction.REFLEXION) {
+            try {
+                if(sport) {
+                    return (VoitureSport) Class.forName("VoitureSport").getDeclaredConstructor(int.class).newInstance(vitesse);
+                } else {
+                    return (Voiture) Class.forName("Voiture").getDeclaredConstructor(int.class).newInstance(vitesse);
+                }
+            } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException classNotFoundException) {
+                classNotFoundException.printStackTrace();
+            }
+        } else if(mode == ModeConstruction.META) {
+            //TODO
+        }
+    };
 }
